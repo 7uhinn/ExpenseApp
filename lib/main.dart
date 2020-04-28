a0484e3356eacaabfc05f74cd3d2a3ca3663eec8
Tuhin <tuhin_sarkar@srmap.edu.in>
2020-04-29 01:09:11 +0530

    lvl 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,6 +15,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> txn = [
+    Transaction(
+      txnid: 1,
+      title: 'Food',
+      amt: 120,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      txnid: 2,
+      title: 'Shoes',
+      amt: 1000,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
